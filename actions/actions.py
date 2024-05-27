541b3301415f111e71da597e6518452e70760648
vanderson <vandersonaugusto6@gmail.com>
2024-05-27 18:16:11 -0300

    last_update_all_working

--- actions/actions.py
+++ actions/actions.py
@@ -7,6 +7,7 @@ from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action
 from rasa_sdk.events import SessionStarted, ActionExecuted, UserUtteranceReverted
+import requests
 
 class ActionShowButtons(Action):
     def name(self):
@@ -30,13 +31,25 @@ class ValidateFormularioFabian(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,     
             ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"nome": slot_value}
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"nome": slot_value}
-
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "nome": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"nome": slot_value}
+            else:
+                return {"nome": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"nome": None} 
+        
+        
     def validate_email(
             self, 
             slot_value: Any,
@@ -44,14 +57,25 @@ class ValidateFormularioFabian(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,     
             ) -> Dict[Text,Any]:
-        # Salvando o slot preenchido
-        if slot_value:
-            print(slot_value)
-            return {"email": slot_value}
+        session_id = tracker.sender_id      
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "email": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"email": slot_value}
+            else:
+                return {"email": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"email": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"email": slot_value}
+        
     def validate_telefone(
             self, 
             slot_value: Any,
@@ -59,14 +83,23 @@ class ValidateFormularioFabian(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,     
             ) -> Dict[Text,Any]:
-        # Salvando o slot preenchido
-        if slot_value:
-            print(slot_value)
-            return {"telefone": slot_value}
-        
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"telefone": slot_value}
+        session_id = tracker.sender_id   
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "phoneNumber": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"telefone": slot_value}
+            else:
+                return {"telefone": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"email": None} 
     
     def validate_instagram(
             self, 
@@ -75,14 +108,22 @@ class ValidateFormularioFabian(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,     
             ) -> Dict[Text,Any]:
-        # Salvando o slot preenchido
-        if slot_value:
-            print(slot_value)
-            return {"instagram": slot_value}
-        
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"instagram": slot_value}
+        session_id = tracker.sender_id   
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "instagram": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"instagram": slot_value}
+            else:
+                return {"instagram": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"email": None} 
     
     def validate_atua_terapeuta(
         self, 
@@ -91,16 +132,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
         ) -> Dict[Text, Any]:
-        # Lista de valores permitidos
-        valid_values = ["Menos de 6 meses", "Entre 7 e 12 meses", "Entre 1 e 2 anos", "Há mais de 2 anos"]
-
-        if slot_value in valid_values:
-            return {"atua_terapeuta": slot_value}
-        else:
-            # Informar o usuário e pedir para escolher uma opção válida
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"atua_terapeuta": None}  # Manter o slot limpo até uma entrada válida ser fornecida
-
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "atua_terapeuta": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"atua_terapeuta": slot_value}
+            else:
+                return {"atua_terapeuta": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"atua_terapeuta": None} 
+        
     def validate_valor_medio(
             self, 
             slot_value: Any,
@@ -108,11 +157,23 @@ class ValidateFormularioFabian(FormValidationAction):
             tracker: Tracker,
             domain: DomainDict,     
             ) -> Dict[Text,Any]:
-        # Salvando o slot preenchido
-        print(slot_value)
-        return {"valor_medio": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "valor_medio": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                return {"valor_medio": slot_value}
+            else:
+                return {"valor_medio": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"nome": None} 
         
-
     def validate_qtd_sessoes(
         self, 
         slot_value: Any,
@@ -120,13 +181,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"qtd_sessoes": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "qtd_sessoes": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"qtd_sessoes": slot_value}
+            else:
+                return {"qtd_sessoes": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"qtd_sessoes": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"qtd_sessoes": slot_value}
     def validate_faturamento_tri(
         self, 
         slot_value: Any,
@@ -134,13 +206,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"faturamento_tri": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "faturamento_tri": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"faturamento_tri": slot_value}
+            else:
+                return {"faturamento_tri": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"faturamento_tri": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"faturamento_tri": slot_value}
     def validate_qtd_sess_perm(
         self, 
         slot_value: Any,
@@ -148,13 +231,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"qtd_sess_perm": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "qtd_sess_perm": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"qtd_sess_perm": slot_value}
+            else:
+                return {"qtd_sess_perm": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"qtd_sess_perm": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"qtd_sess_perm": slot_value}
     def validate_mtd_proprio(
         self, 
         slot_value: Any,
@@ -162,13 +256,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"mtd_proprio": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "mtd_proprio": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"mtd_proprio": slot_value}
+            else:
+                return {"mtd_proprio": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"mtd_proprio": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"mtd_proprio": slot_value}
     def validate_optd_sem(
         self, 
         slot_value: Any,
@@ -176,13 +281,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"optd_sem": slot_value}
-    
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"optd_sem": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "optd_sem": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"optd_sem": slot_value}
+            else:
+                return {"optd_sem": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"optd_sem": None} 
+        
     def validate_vnd_sem(
         self, 
         slot_value: Any,
@@ -190,13 +306,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"vnd_sem": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "vnd_sem": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"vnd_sem": slot_value}
+            else:
+                return {"vnd_sem": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"vnd_sem": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"vnd_sem": slot_value}
     def validate_invts_ads(
         self, 
         slot_value: Any,
@@ -204,12 +331,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"invts_ads": slot_value}
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"invts_ads": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "invts_ads": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"invts_ads": slot_value}
+            else:
+                return {"invts_ads": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"invts_ads": None} 
+        
 
     def validate_avg_invts(
         self, 
@@ -218,13 +357,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"avg_invts": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "avg_invts": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"avg_invts": slot_value}
+            else:
+                return {"avg_invts": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"avg_invts": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"avg_invts": slot_value}
     def validate_cost_cust(
         self, 
         slot_value: Any,
@@ -232,13 +382,24 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"cost_cust": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "cost_cust": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"cost_cust": slot_value}
+            else:
+                return {"cost_cust": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"cost_cust": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"cost_cust": slot_value}
 
     def validate_roas(
         self, 
@@ -247,11 +408,22 @@ class ValidateFormularioFabian(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,     
         ) -> Dict[Text,Any]:
-        if slot_value:
-            print(slot_value)
-            return {"roas": slot_value}
+        session_id = tracker.sender_id
+        url = "http://localhost:3000/usuario/cadastro"
+        data = {
+            "session_id": session_id,
+            "roas": slot_value,
+        }
+        try:
+            response = requests.post(url, json=data)
+            if 200 <= response.status_code < 300:
+                print(slot_value)
+                return {"roas": slot_value}
+            else:
+                return {"roas": None}
+                
+        except requests.exceptions.RequestException as e:
+            print(e)  
+            return {"roas": None} 
         
-        else:
-            dispatcher.utter_message(text="Por favor, escolha uma das opções fornecidas.")
-            return {"roas": slot_value}
 
